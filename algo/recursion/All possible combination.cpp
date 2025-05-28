2e23f4ea8bc3112f2314efb8a3ab6d3c5d8c8709
Arham Apon <arhamaponutsho@gmail.com>
2025-05-28 07:42:15 +0600

    slgo quiz

--- algo/recursion/All possible combination.cpp
+++ algo/recursion/All possible combination.cpp
@@ -1,55 +1,34 @@
-#include <bits/stdc++.h>
-using namespace std;
+    #include<bits/stdc++.h>
+    using namespace std;
 
-// Helper function to find all combinations
-// of size r in an array of size n
-void combinationUtil(int ind, int r, vector<int> &data,
-    vector<vector<int>> &result, vector<int> &arr) {
-    int n = arr.size();
-
-    // If size of current combination is r
-    if (data.size() == r) {
-        result.push_back(data);
+void perm(vector<vector<int>> &res, vector<int> &arr, int idx) {
+    if (idx == arr.size()) {
+        res.push_back(arr);
         return;
     }
-
-    // Replace index with all possible elements
-    for(int i = ind; i < n; i++) {
-
-        // Current element is included
-        data.push_back(arr[i]);
-
-        // Recur for next elements
-        combinationUtil(i + 1, r, data, result, arr);
-
-        // Backtrack to find other combinations
-        data.pop_back();
+    for (int i = idx; i < arr.size(); i++) {
+        swap(arr[idx], arr[i]);
+        perm(res, arr, idx + 1);
+        swap(arr[idx], arr[i]);
     }
 }
 
-// Function to find all combinations of size r
-// in an array of size n
-vector<vector<int>> findCombination(vector<int> &arr, int r) {
-    int n = arr.size();
+vector<vector<int>> permuteUnique(vector<int> arr) {
+    vector<vector<int>> res;
+    perm(res, arr, 0);
+    return res;
 
-    // to store the result
-    vector<vector<int>> result;
-
-    // Temporary array to store current combination
-    vector<int> data;
-    combinationUtil(0, r, data, result, arr);
-    return result;
 }
 
-int main()  {
-    vector<int> arr = {1, 2, 3, 4};
-    int r = 2;
-    vector<vector<int>> res = findCombination(arr, r);
-    for (const auto &comb : res) {
-        for (int num : comb) {
-            cout << num << " ";
+
+    int main() {
+    vector<int> arr = { 1, 2, 3,4,5 };
+    vector<vector<int>> res = permuteUnique(arr);
+    for (auto x : res) {
+        for (auto y : x) {
+            cout << y << " ";
         }
         cout << endl;
     }
-    return 0;
-}
+    }
+
